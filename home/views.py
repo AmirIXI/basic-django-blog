bdc795d8b6db426ed7136eae8665d0aabe6912c2
IXI.Amir <amiraliardeshiri69@gmail.com>
2024-10-25 00:39:38 +0330

    update home views.py and add posts on it

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.views import View
+from .models import Post
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+        context = {
+            'posts': Post.objects.all()
+        }
+        return render(request, 'home/index.html', context)
